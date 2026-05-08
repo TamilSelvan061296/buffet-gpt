bf7b01a4a4b986dcd3e36be0a7aae0ec0e896a8b
Tamil Selvan Murugesan <tselvan482@gmail.com>
2026-05-08 09:17:39 +0000

    Added chunking, logger and config files

--- src/scripts/data_ingestor.py
+++ src/scripts/data_ingestor.py
@@ -1,11 +1,14 @@
 from pathlib import Path
+import logging
 
 from langchain_community.document_loaders import BSHTMLLoader
 from langchain_core.documents import Document
 from langchain_pymupdf4llm import PyMuPDF4LLMLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+logger = logging.getLogger(__name__)
 
-class html_loader:
+class HtmlLoader:
     """
     Given the html path folder, Uses langchain's document loader methods to load all
     the html files recursively into documents.
@@ -16,13 +19,16 @@ class html_loader:
     def load_html_files(self) -> list[Document]:
         docs: list[Document] = []
         for path in sorted(Path(self.html_folder_path).rglob("*.html")):
-            loader = BSHTMLLoader(str(path), open_encoding="windows-1252")
-            for doc in loader.load():
-                doc.metadata["year"] = int(path.stem)
-                docs.append(doc)
+            try:
+                loader = BSHTMLLoader(str(path), open_encoding="windows-1252")
+                for doc in loader.load():
+                    doc.metadata["year"] = int(path.stem)
+                    docs.append(doc)
+            except:
+                logger.error(f"Error while parsing {path}")
         return docs
 
-class pdf_loader:
+class PdfLoader:
     """
     Given the pdf path folder, Uses langchain's document loader methods to load all
     the path files recursively into documents.
@@ -33,8 +39,28 @@ class pdf_loader:
     def load_pdf_files(self) -> list[Document]:
         docs: list[Document] = []
         for path in sorted(Path(self.pdf_folder_path).rglob("*.pdf")):
-            loader = PyMuPDF4LLMLoader(str(path), mode="single")
-            for doc in loader.load():
-                doc.metadata["year"] = int(path.stem)
-                docs.append(doc)
+            try:
+                loader = PyMuPDF4LLMLoader(str(path), mode="single")
+                for doc in loader.load():
+                    doc.metadata["year"] = int(path.stem)
+                    docs.append(doc)
+            except:
+                logger.error(f"Error while parsing {path}")
         return docs
+
+
+class Chunker:
+    def __init__(self, chunk_size: int, chunk_overlap: int):
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+
+    def chunk_docs(self, docs: list[Document]):
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap,
+            add_start_index=True,
+        )
+
+        all_chunks = text_splitter.split_documents(docs)
+
+        return all_chunks
--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,5 +1,53 @@
+import logging
+from pathlib import Path
+
+import yaml
+from langchain_core.documents import Document
+
+from data_ingestor import HtmlLoader, PdfLoader, Chunker
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='logs/app.log',
+    filemode='w'
+)
+
+CONFIG_PATH = Path(__file__).parent / "config.yaml"
+
+
+def load_config() -> dict:
+    with CONFIG_PATH.open() as f:
+        return yaml.safe_load(f)
+
+
 def main():
-    print("Hello from buffet-gpt!")
+    config = load_config()
+    paths = config["data_folder_paths"]
+    chunking = config["chunking"]
+
+    docs: list[Document] = []
+
+    # load the html docs
+    logging.info("Loading HTML files..")
+    html = HtmlLoader(paths["html_folder_path"])
+    html_docs = html.load_html_files()
+    docs.extend(html_docs)
+
+    # load the pdf docs
+    logging.info("Loading PDF files")
+    pdf = PdfLoader(paths["pdf_folder_path"])
+    pdf_docs = pdf.load_pdf_files()
+    docs.extend(pdf_docs)
+
+    # chunk the docs
+    logging.info("Creating chunks")
+    c = Chunker(
+        chunk_size=chunking["chunk_size"],
+        chunk_overlap=chunking["chunk_overlap"],
+    )
+    chunks = c.chunk_docs(docs=docs)
+
 
 
 if __name__ == "__main__":
